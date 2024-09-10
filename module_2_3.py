f9ac1c293ab9e69008e93114d865e318ba36a4c9
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-09-10 08:06:29 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -12,6 +12,8 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 count = 0
 
 while count < len(my_list):
+    if my_list[count] == 0:
+        count += 1
     if my_list[count] >= 0:
         print(my_list[count])
         count += 1
