3bd8bb3119d68ea27f8b8a0ba45f0c48bd5ad19b
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-09-10 08:03:11 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,22 @@
+# Дан список чисел [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+# Нужно выписывать из этого списка только положительные числа до тех пор,
+# пока не встретите отрицательное или не закончится список (выход за границу).
+
+# lst = ['a', 'b', 'c']
+# count = 0
+# while count < len(lst):
+# 	print(lst[count])
+# 	count += 1
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+
+while count < len(my_list):
+    if my_list[count] >= 0:
+        print(my_list[count])
+        count += 1
+        continue
+    if my_list[count] < 0:
+        break
+
+
