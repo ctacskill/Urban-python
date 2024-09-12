06f49de63b82f3760990cb30c688747dfe5c5455
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-09-12 12:57:03 +0800

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,30 @@
+# Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Используя этот список составьте второй список primes содержащий только простые числа.
+# А так же третий список not_primes, содержащий все не простые числа.
+# Выведите списки primes и not_primes на экран(в консоль).
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for i in numbers:
+    if i > 1:
+        for k in range(2,i):
+            if i % k == 0:
+                not_primes.append(i)
+                break
+        else:
+            primes.append(i)
+
+print(primes)
+print(not_primes)
+
+
+
+
+
+
+
+
+
+
