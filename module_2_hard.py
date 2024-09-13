f38edcfb14d0107917bc1e017e9d822aa320b476
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-09-13 15:59:01 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,7 +3,6 @@ import random
 first_num = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16, 17, 18, 19, 20]
 
 def password():
-    password_ = []
     result = ''
     n = random.choice(first_num)
     print(n)
@@ -14,8 +13,6 @@ def password():
             else:
                 sum = i + k
             if n % sum == 0:
-                password_.append(i)
-                password_.append(k)
                 result= result + str(i) + str(k)
                 
     print(result)
