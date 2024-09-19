806ec534693a553478d577ca2cfc614f843f4c9a
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-09-19 17:50:37 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -26,9 +26,10 @@ print(result2)
 
 def single_root_words(root_word, *other_words):
     same_words = []
-    count = 0
     for word in other_words:
         if root_word.lower() in word.lower():
+            same_words.append(word)
+        elif word.lower() in root_word.lower():
             same_words.append(word)
     print(same_words)
 
