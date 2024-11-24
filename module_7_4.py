7b691ff66fadb71d9d11d69580fbc18eb976c54d
ctacskill <165298450+ctacskill@users.noreply.github.com>
2024-11-24 04:33:04 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,18 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+challenge_result = 'Победа команды Волшебники данных!'
+
+
+print('В команде Мастера кода учатсников: %s!' % team1_num)
+print('Итого сегодня в командах участников: %s и %s!' % (team1_num, team2_num))
+print( 'Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {} с!'.format(round(team2_time, 1)))
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'результат битвы: {challenge_result}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
