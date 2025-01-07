a3546a359d715ee009b6ef51cb1c78cb0ddbe01d
ctacskill <165298450+ctacskill@users.noreply.github.com>
2025-01-07 14:54:17 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -1,2 +1,24 @@
-import sys
-print(sys.version)
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+
+bot = Bot(token= api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(commands= ['start'])
+async def start(message):
+    print('Привет, я бот помогающий твоему здоровью!')
+
+
+@dp.message_handler()
+async def all_messeges(messege):
+    print('Введите команду /start для начала общения!')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp,skip_updates=True)
+
+
+
